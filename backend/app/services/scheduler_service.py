60ecdda4e68505f2556384fdcdd42364540d7397
Jahnavi-Mandadi <mandadijahnavi1909@gmail.com>
2026-05-30 10:40:46 +0530

    Day 13 completed - Scheduler discovers active DAGs

--- backend/app/services/scheduler_service.py
+++ backend/app/services/scheduler_service.py
@@ -2,16 +2,37 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
 
-def check_scheduled_dags():
-    print("Checking scheduled DAGs...")
 
-def start_scheduler():
+def check_scheduled_dags(app):
+
+    with app.app_context():
+
+        from app.models.dag_model import Dag
+
+        print("Checking scheduled DAGs...")
+
+        dags = Dag.query.filter_by(
+            is_active=True
+        ).all()
+
+        print(f"Found {len(dags)} active DAGs")
+
+        for dag in dags:
+
+            print(
+                f"DAG ID={dag.id}, Name={dag.name}"
+            )
+
+
+def start_scheduler(app):
+
     print("start_scheduler called")
 
     scheduler.add_job(
         check_scheduled_dags,
         "interval",
-        seconds=10
+        seconds=10,
+        args=[app]
     )
 
     scheduler.start()
--- backend/run.py
+++ backend/run.py
@@ -12,11 +12,11 @@ app=create_app()
 
 with app.app_context():
     db.create_all()
+    start_scheduler(app)
 
 
 if __name__=="__main__":
-    start_scheduler()
-
+    
     app.run(
         host="0.0.0.0",
         port=5000,
